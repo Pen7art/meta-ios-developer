2bf4bb86eff6bea0161c29451d3e1f2e3195c3ab
Cristian Duciuc <cduciuc@sorint.com>
2023-03-27 16:57:31 +0200

    Initial Commit

---
+++ Meta ios developer/Meta_ios_developerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meta_ios_developerApp.swift
+//  Meta ios developer
+//
+//  Created by Cristian Duciuc on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Meta_ios_developerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
